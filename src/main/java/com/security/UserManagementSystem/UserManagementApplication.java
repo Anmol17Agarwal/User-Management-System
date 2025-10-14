7a1cddfe96d6e906c6f4be8bd76d55241c374ee3
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-14 18:59:56 +0530

    removed exclude annotation of datasource and jpa

--- src/main/java/com/security/UserManagementSystem/UserManagementApplication.java
+++ src/main/java/com/security/UserManagementSystem/UserManagementApplication.java
@@ -3,10 +3,7 @@ package com.security.UserManagementSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-		org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
