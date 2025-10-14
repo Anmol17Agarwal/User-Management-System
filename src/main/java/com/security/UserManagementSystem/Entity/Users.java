a9a28eda0fba132c9ae4283eab66adcc31dc2016
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-10-14 19:02:09 +0530

    created user entity

--- src/main/java/com/security/UserManagementSystem/Entity/Users.java
+++ src/main/java/com/security/UserManagementSystem/Entity/Users.java
@@ -1,11 +1,22 @@
 package com.security.UserManagementSystem.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Users {
+    @Id
     private Long userId;
     private String userName;
-
     private String userPassword;
     private String userRole;
+
+    public Users(){}
 
     public Users(Long userId, String userName, String userPassword, String userRole) {
         this.userId = userId;
@@ -13,6 +24,7 @@ public class Users {
         this.userPassword = userPassword;
         this.userRole = userRole;
     }
+
     public Long getUserId() {
         return userId;
     }
@@ -29,14 +41,6 @@ public class Users {
         this.userName = userName;
     }
 
-    public String getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(String userRole) {
-        this.userRole = userRole;
-    }
-
     public String getUserPassword() {
         return userPassword;
     }
@@ -45,13 +49,13 @@ public class Users {
         this.userPassword = userPassword;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId=" + userId +
-                ", userName='" + userName + '\'' +
-                ", userPassword='" + userPassword + '\'' +
-                ", userRole='" + userRole + '\'' +
-                '}';
+    public String getUserRole() {
+        return userRole;
     }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+
 }
